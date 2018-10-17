7edefd33634f0795cdc2f4a2c9cefe2c057b8b57
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2018-10-17 14:03:16 +0530

    new changes

--- ProgressTut/ViewController.swift
+++ ProgressTut/ViewController.swift
@@ -9,11 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var bnt: UIButton!
+    
+    var isRed = false
+    var progressBarTimer: Timer!
+    var isRunning = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        progressView.progress = 0.0
+        progressiveViewChanges()
     }
+    
+    //MARK: making progressive View changes
+    func progressiveViewChanges() {
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 57)
+        progressView.layer.cornerRadius = 10
+        progressView.clipsToBounds = true
+        progressView.layer.sublayers![1].cornerRadius = 10
+        progressView.subviews[1].clipsToBounds = true
+    }
+    
+    @IBAction func bnt(_ sender: Any) {
+        
+        if isRunning {
+          progressBarTimer.invalidate()
+          bnt.setTitle("Start", for: .normal)
+        } else {
+            bnt.setTitle("Stop", for: .normal)
+            progressView.progress = 0.0
+            setTimerForProgressiveBar()
+        }
+        
+        changedBarColor()
+        isRunning = !isRunning
+    }
+    
+    //MARK:  Set Timer for progressive Bar
+    func setTimerForProgressiveBar() {
+        self.progressBarTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.updateProgressView), userInfo: nil, repeats: true)
+    }
+    
+    
+    func changedBarColor() {
+        if(isRed){
+            progressView.progressTintColor = UIColor.blue
+            progressView.progressViewStyle = .default
+        } else{
+            progressView.progressTintColor = UIColor.red
+            progressView.progressViewStyle = .bar
+        }
+    }
+    
+    @objc func updateProgressView(){
+        
+        progressView.progress += 0.1
+        UIView.animate(withDuration: 3, animations: { () -> Void in
+            self.progressView.setProgress(1.0, animated: true)
+        })
+    
+        if(progressView.progress == 1.0)
+        {
+            progressBarTimer.invalidate()
+            isRunning = false
+            bnt.setTitle("Start", for: .normal)
+        }
+    }
+    
 
 
 }
