1625df1f4f0a8380e1fce463bcfba40537fc7113
Anup Sukumaran <anup.sukumaran9@gmail.com>
2018-10-18 10:17:04 +0530

    new

---
+++ ProgressTut/UIProgressBar+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  UIProgressBar+Extension.swift
+//  EV Quiz
+//
+//  Created by Sukumar Anup Sukumaran on 18/10/18.
+//  Copyright © 2018 WIS. All rights reserved.
+//
+
+import UIKit
+
+extension UIProgressView {
+    
+    open override func layoutSubviews() {
+//        let maskLayerPath = UIBezierPath(roundedRect: bounds, cornerRadius: 4.0)
+//        let maskLayer = CAShapeLayer()
+//        maskLayer.frame = self.bounds
+//        maskLayer.path = maskLayerPath.cgPath
+//        layer.mask = maskLayer
+    }
+    
+}
+
+//if incre <= CGFloat(dividedNum) {
+//    incre += 0.1
+//    print("Incre = \(incre)")
+//    self.rectProgress(incremented: incre)
+//} else {
+//    progressBarTimer.invalidate()
+//}
--- ProgressTut/ViewController.swift
+++ ProgressTut/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var bnt: UIButton!
     @IBOutlet weak var totalPeopleVoted: UITextField!
     @IBOutlet weak var numOfPeopleVotedForOneBox: UITextField!
+    @IBOutlet weak var viewProg: UIView!
     
     
     var pepVotedForOneBox:Float = 0.0
@@ -22,11 +23,57 @@ class ViewController: UIViewController {
     var progressBarTimer: Timer!
     var isRunning = false
     
+    let viewCornerRadius : CGFloat = 5
+    var borderLayer : CAShapeLayer = CAShapeLayer()
+    let progressLayer : CAShapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         progressiveViewChanges()
         totalPeopleVoted.text = "1000"
         numOfPeopleVotedForOneBox.text = "555"
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        drawProgressLayer()
+    }
+    
+    func drawProgressLayer(){
+        
+        let bezierPath = UIBezierPath(roundedRect: viewProg.bounds, cornerRadius: viewCornerRadius)
+        bezierPath.close()
+        borderLayer.path = bezierPath.cgPath
+        borderLayer.fillColor = UIColor.black.cgColor
+        borderLayer.strokeEnd = 0
+        viewProg.layer.addSublayer(borderLayer)
+        
+        
+    }
+    
+    //Make sure the value that you want in the function `rectProgress` that is going to define
+    //the width of your progress bar must be in the range of
+    // 0 <--> viewProg.bounds.width - 10 , reason why to keep the layer inside the view with some border left spare.
+    //if you are receiving your progress values in 0.00 -- 1.00 range , just multiply your progress values to viewProg.bounds.width - 10 and send them as *incremented:* parameter in this func
+    
+    func rectProgress(incremented : CGFloat){
+        
+       // print("incremented = \(incremented)")
+        if incremented <= viewProg.bounds.width - 10{
+            progressLayer.removeFromSuperlayer()
+            let rect = CGRect(x: 5, y: 5, width: incremented, height: viewProg.bounds.height - 10)
+            let bezierPathProg = UIBezierPath(roundedRect: rect , cornerRadius: viewCornerRadius)
+            bezierPathProg.close()
+            
+            progressLayer.path = bezierPathProg.cgPath
+            progressLayer.fillColor = UIColor.white.cgColor
+            borderLayer.addSublayer(self.progressLayer)
+            
+           
+            
+        }
+        
     }
     
     //MARK: making progressive View changes
@@ -57,11 +104,29 @@ class ViewController: UIViewController {
         
         changedBarColor()
         isRunning = !isRunning
+        
+//        let dividedNum = calcuForProgressView(inputNum: pepVotedForOneBox)
+//        let width = viewProg.bounds.width
+//        let incre = CGFloat(dividedNum) * ((width) - 10)
+//        print(" incre = \(incre)😩")
+//        // progressView.progress += dividedNum
+//
+//    //    UIView.animate(withDuration: 3) {
+//            for i:CGFloat in stride(from: CGFloat(0), to: incre, by: 1) {
+//                print("i = \(i)😄")
+//
+//                self.rectProgress(incremented: i)
+//
+//
+//            }
+//       // }
+        
+        
     }
     
     //MARK:  Set Timer for progressive Bar
     func setTimerForProgressiveBar() {
-        self.progressBarTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.updateProgressView), userInfo: nil, repeats: true)
+        self.progressBarTimer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(ViewController.updateProgressView), userInfo: nil, repeats: true)
     }
     
     
@@ -82,21 +147,38 @@ class ViewController: UIViewController {
         print("newNum = \(newNum)")
         return newNum
     }
+    var incre: CGFloat = 0.0
     
     @objc func updateProgressView(){
         let dividedNum = calcuForProgressView(inputNum: pepVotedForOneBox)
         print("dividedNum = \(dividedNum)")
-       // progressView.progress += dividedNum
+        
+       
+        
         UIView.animate(withDuration: 3, animations: { () -> Void in
          //   self.progressView.progress = dividedNum
+            
+            
             self.progressView.setProgress(dividedNum, animated: true)
         })
     
+      
         if(progressView.progress == 1.0)
         {
-            progressBarTimer.invalidate()
+            
             isRunning = false
             bnt.setTitle("Start", for: .normal)
+        }
+        
+        let width = viewProg.bounds.width
+        let newVal = CGFloat(dividedNum) * ((width) - 10)
+        
+        if incre <= CGFloat(newVal) {
+            incre += 1
+            print("Incre = \(incre)")
+            self.rectProgress(incremented: incre)
+        } else {
+              progressBarTimer.invalidate()
         }
     }
     
@@ -105,7 +187,12 @@ class ViewController: UIViewController {
         self.view.endEditing(true)
     }
     
-
+    var increment: CGFloat = 0.0
+    @IBAction func addAction(_ sender: Any) {
+        increment += 1.0
+        self.rectProgress(incremented: increment)
+    }
+    
 
 }
 
